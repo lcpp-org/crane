e7ed783e6fb3e6f81cd63f793d9123a2adea921c
Shane Keniley <keniley1@illinois.edu>
2020-07-21 15:37:49 -0500

    Replaces DataReadScalar with Spline and Linear interpolation classes

--- include/auxkernels/ScalarSplineInterpolation.h
+++ include/auxkernels/ScalarSplineInterpolation.h
@@ -16,26 +16,25 @@
 
 #include "AuxScalarKernel.h"
 #include "SplineInterpolation.h"
-#include "LinearInterpolation.h"
 
-class DataReadScalar;
+class ScalarSplineInterpolation;
 
 template <>
-InputParameters validParams<DataReadScalar>();
+InputParameters validParams<ScalarSplineInterpolation>();
 
-class DataReadScalar : public AuxScalarKernel
+class ScalarSplineInterpolation : public AuxScalarKernel
 {
 public:
-  DataReadScalar(const InputParameters & parameters);
+  ScalarSplineInterpolation(const InputParameters & parameters);
 
 protected:
   virtual Real computeValue();
   SplineInterpolation _coefficient_interpolation;
-  // LinearInterpolation _coefficient_interpolation_linear;
   const VariableValue & _sampler_var;
   Real _sampler_const;
   std::string _sampling_format;
   bool _use_time;
   bool _use_log;
+  std::string _interpolation_type;
   Real _scale_factor;
 };
--- src/auxkernels/ScalarLinearInterpolation.C
+++ src/auxkernels/ScalarLinearInterpolation.C
@@ -12,29 +12,36 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 
-#include "DataReadScalar.h"
+#include "ScalarLinearInterpolation.h"
 
-registerMooseObject("CraneApp", DataReadScalar);
+registerMooseObject("CraneApp", ScalarLinearInterpolation);
 
 template <>
 InputParameters
-validParams<DataReadScalar>()
+validParams<ScalarLinearInterpolation>()
 {
   InputParameters params = validParams<AuxScalarKernel>();
   params.addCoupledVar("sampler", 0, "The variable with which the data will be sampled.");
   params.addParam<bool>("use_time", false, "Whether or not to sample with time.");
-  params.addParam<bool>("use_log", false, "Whether or not to return the natural logarithm of the sampled data.");
-  params.addParam<Real>("scale_factor", 1.0, "Multiplies the sampled output by a given factor. Convert from m^3 to cm^3, for example.(Optional)");
+  params.addParam<bool>(
+      "use_log", false, "Whether or not to return the natural logarithm of the sampled data.");
+  params.addParam<Real>("scale_factor",
+                        1.0,
+                        "Multiplies the sampled output by a given factor. Convert from m^3 to "
+                        "cm^3, for example.(Optional)");
   params.addParam<Real>("const_sampler", 0, "The value with which the data will be sampled.");
-  params.addParam<FileName>("property_file", "",
-      "The file containing interpolation tables for material properties.");
-  params.addParam<std::string>("file_location", "", "The name of the file that stores the reaction rate tables.");
-  params.addParam<std::string>("sampling_format", "reduced_field",
-    "The format that the rate constant files are in. Options: reduced_field and electron_energy.");
+  params.addParam<FileName>(
+      "property_file", "", "The file containing interpolation tables for material properties.");
+  params.addParam<std::string>(
+      "file_location", "", "The name of the file that stores the reaction rate tables.");
+  params.addParam<std::string>("sampling_format",
+                               "reduced_field",
+                               "The format that the rate constant files are in. Options: "
+                               "reduced_field and electron_energy.");
   return params;
 }
 
-DataReadScalar::DataReadScalar(const InputParameters & parameters)
+ScalarLinearInterpolation::ScalarLinearInterpolation(const InputParameters & parameters)
   : AuxScalarKernel(parameters),
     _sampler_var(coupledScalarValue("sampler")),
     _sampler_const(getParam<Real>("const_sampler")),
@@ -45,7 +52,8 @@ DataReadScalar::DataReadScalar(const InputParameters & parameters)
 {
   std::vector<Real> x_val;
   std::vector<Real> y_val;
-  std::string file_name = getParam<std::string>("file_location") + "/" + getParam<FileName>("property_file");
+  std::string file_name =
+      getParam<std::string>("file_location") + "/" + getParam<FileName>("property_file");
   MooseUtils::checkFileReadable(file_name);
   const char * charPath = file_name.c_str();
   std::ifstream myfile(charPath);
@@ -65,31 +73,38 @@ DataReadScalar::DataReadScalar(const InputParameters & parameters)
     mooseError("Unable to open file");
 
   _coefficient_interpolation.setData(x_val, y_val);
-  // _coefficient_interpolation_linear.setData(x_val, y_val);
 }
 
 Real
-DataReadScalar::computeValue()
+ScalarLinearInterpolation::computeValue()
 {
-  Real val;
-  if (isCoupledScalar("sampler"))
-    val = _coefficient_interpolation.sample(_sampler_var[_i]);
-  else if (!isCoupledScalar("sampler") && _use_time)
-    // val = _coefficient_interpolation_linear.sample(_t);
-    val = _coefficient_interpolation.sample(_t);
-  else
-    val = _coefficient_interpolation.sample(_sampler_const);
+  Real val = 0;
 
-  // Ensure positivity
-  if (val < 0.0)
+  try
   {
-    val = 0.0;
-  }
-  val = val * _scale_factor;
+    if (isCoupledScalar("sampler"))
+      val = _coefficient_interpolation.sample(_sampler_var[_i]);
+    else if (!isCoupledScalar("sampler") && _use_time)
+      // val = _coefficient_interpolation_linear.sample(_t);
+      val = _coefficient_interpolation.sample(_t);
+    else
+      val = _coefficient_interpolation.sample(_sampler_const);
 
-  if (_use_log && val>0)
+    // Ensure positivity
+    if (val < 0.0)
+    {
+      val = 0.0;
+    }
+    val = val * _scale_factor;
+  }
+  catch (const std::exception & err)
+  {
+    MooseException("Linear interpolation out of range! Cutting the timestep...");
+  }
+
+  if (_use_log && val > 0)
     return log(val);
-  else if (_use_log && val<=0)
+  else if (_use_log && val <= 0)
     return 1e-4;
   else
     return val;
