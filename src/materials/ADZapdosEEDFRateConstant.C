3843adfc37514906d60d326b95dcb207952a3921
Casey Icenhour <cticenho@ncsu.edu>
2021-06-01 09:23:04 -0600

    Restore positivity restraint to ADZapdosEEDFRateConstant

--- src/materials/ADZapdosEEDFRateConstant.C
+++ src/materials/ADZapdosEEDFRateConstant.C
@@ -68,4 +68,10 @@ ADZapdosEEDFRateConstant::computeQpProperties()
                                              std::exp(_mean_en[_qp].value() - _em[_qp].value())) *
                                          std::exp(_mean_en[_qp].value() - _em[_qp].value()) *
                                          (_mean_en[_qp].derivatives() - _em[_qp].derivatives());
+
+  if (_rate_coefficient[_qp].value() < 0.0)
+  {
+    _rate_coefficient[_qp].value() = 0.0;
+    _rate_coefficient[_qp].derivatives() = 0.0;
+  }
 }
