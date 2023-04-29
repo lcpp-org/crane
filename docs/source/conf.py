a19054c982ed217c28e156c8af3934f3f90f5740
Davide Curreli <dcurreli@illinois.edu>
2023-04-29 15:45:35 -0500

    Specify configure bibtex bibfiles in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,8 @@ extensions = [
     'sphinx_copybutton'
 ]
 
+bibtex_bibfiles = ['refs.bib']
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
